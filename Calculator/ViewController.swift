666f113641610ee5ad9f839bf07a645dfe7f5ed3
urassh <84073765+urassh@users.noreply.github.com>
2021-10-21 20:54:08 +0900

    STEP:1 1, made 'collectionView'  and 'Label' with 'main.storyboard'. 2, I add some variable to 'ViewController.swift' 3, I made the initial setting for 'collectionView'

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calculatorCollectionView: UICollectionView!
+    @IBOutlet weak var calculatorHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("コミットテスト")
+        calculatorCollectionView.delegate = self
+        calculatorCollectionView.dataSource = self
+        calculatorCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "sellId")
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = calculatorCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
 }
 
